1231757c135f9a86d4bc6972d255b5371dc03ad6
JehudaRapha <130199908+JehudaRapha@users.noreply.github.com>
2024-09-03 14:28:29 +0700

    init

--- Smart Door/NilaiStateSensor/NilaiStateSensor.ino
+++ Smart Door/NilaiStateSensor/NilaiStateSensor.ino
@@ -1,23 +1,16 @@
-const int relayPin = D2;
-#define sensor D3
+#define MAGNETIC_SENSOR_PIN D3
 
 void setup() {
   // Inisialisasi serial monitor untuk debugging
   Serial.begin(9600);
 
   // Set pin D3 sebagai input untuk membaca status sensor
-  pinMode(sensor, INPUT);
+  pinMode(MAGNETIC_SENSOR_PIN, INPUT);
 }
 
 void loop() {
-  int sensorValue = digitalRead(sensor);
-  Serial.print("Sensor Value: ");
-  Serial.println(sensorValue);
-
-  if (sensorValue == LOW) {
-    digitalWrite(relayPin, HIGH); // Misalnya, hidupkan relay jika sensor LOW
-  } else {
-    digitalWrite(relayPin, LOW); // Matikan relay jika sensor HIGH
-  }
-  delay(1000);
+  // Membaca status dari sensor magnetik
+  int sensorState = digitalRead(MAGNETIC_SENSOR_PIN);
+  Serial.println(sensorState);
+  delay(500); 
 }
